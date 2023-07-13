cf678b362b883402031fdafbfa8031263d1847e8
Chaitanya Anand <92747604+UltigendLemate@users.noreply.github.com>
2023-07-13 23:52:48 +0530

    q1 krlo guys

--- q1.cpp
+++ q1.cpp
@@ -6,27 +6,27 @@ string s;
 int sum=0;
 int z=1e9+7;
 int dp[27][100001][27];
-vector<int>freq; //hashmap
+vector<int>frequency; //hashmap
 
-int rec(int level,int val,int cnt)
+int helper(int i,int val,int count)
 {
-    if(level==26)
+    if(i==26)
     {
-        if(cnt==k && sum==val)
+        if(count==k && sum==val)
         return 1;
         return 0;
     }
 
-    if(dp[level][val][cnt]!=-1)
-    return dp[level][val][cnt];
+    if(dp[i][val][count]!=-1)
+    return dp[i][val][count];
 
     int ans=0;
-    if(freq[level]==0)
-    ans=(rec(level+1,val,cnt))%z;
+    if(frequency[i]==0)
+    ans=(helper(i+1,val,count))%z;
     else
-    ans=(((rec(level+1,val,cnt))%z)+(((freq[level]%z)*rec(level+1,val+freq[level],cnt+1)%z)%z))%z;
+    ans=(((helper(i+1,val,count))%z)+(((frequency[i]%z)*helper(i+1,val+frequency[i],count+1)%z)%z))%z;
     ans%=z;
-    return dp[level][val][cnt]=ans;
+    return dp[i][val][count]=ans;
 }
 
  
@@ -34,12 +34,12 @@ void solve(){
 
     cin>>n>>k;
     cin>>s;
-    freq.assign(26,0);
+    frequency.assign(26,0);
     for(int i=0;i<n;i++)
-    freq[s[i]-'a']++; //hashmap calculating
+    frequency[s[i]-'a']++; //hashmap calculating
    
-    sort(freq.begin(),freq.end());
-    reverse(freq.begin(),freq.end()); //descending order
+    sort(frequency.begin(),frequency.end());
+    reverse(frequency.begin(),frequency.end()); //descending order
    
     if(k>26) // we have 26 alphabets only. cant make a string of len >26 with unique alphabets
     {
@@ -47,15 +47,15 @@ void solve(){
         return;
     }
 
-    if(freq[k-1]==0) //where we have less than k unique characters. for ex: aabbaaab and k=5, 
-      //here only freq[0] and freq[1] will be non zero numbers, thus freq[k-1] will be 0
+    if(frequency[k-1]==0) //where we have less than k unique characters. for ex: aabbaaab and k=5, 
+      //here only frequency[0] and frequency[1] will be non zero numbers, thus frequency[k-1] will be 0
     {
         cout<<0<<"\n";
         return;
     }
     
     sum=0; //global variable
-    for(int i=0;i<k;i++)sum+=freq[i]; 
+    for(int i=0;i<k;i++)sum+=frequency[i]; 
     for(int i=0;i<=26;i++)
     {
         for(int j=0;j<=n;j++)
@@ -66,7 +66,7 @@ void solve(){
             }
         }
     }
-    int ans=rec(0,0,0);
+    int ans=helper(0,0,0);
     cout<<ans<<"\n";
 }
  
